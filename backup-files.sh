c101f7f8af70a770fddb8552a7d2165721505b55
Maarten Balliauw <maarten@maartenballiauw.be>
2025-04-16 17:05:53 +0200

    Update JetBrains Rider and WebStorm version in backup script

--- backup-files.sh
+++ backup-files.sh
@@ -7,8 +7,8 @@ ROOT_DIR=/Users/maartenba
 mkdir backup
 tar -cf backup/files.tar --exclude "node_modules" \
   "$ROOT_DIR/Desktop" \
-  "$ROOT_DIR/Library/Application Support/JetBrains/Rider2024.3/options/recentSolutions.xml" \
-  "$ROOT_DIR/Library/Application Support/JetBrains/WebStorm2024.3/options/recentSolutions.xml" \
+  "$ROOT_DIR/Library/Application Support/JetBrains/Rider2025.1/options/recentSolutions.xml" \
+  "$ROOT_DIR/Library/Application Support/JetBrains/WebStorm2025.1/options/recentSolutions.xml" \
   "$ROOT_DIR/Library/Containers/com.utmapp.UTM/Data/Documents" \
   "$ROOT_DIR/Projects"
 
